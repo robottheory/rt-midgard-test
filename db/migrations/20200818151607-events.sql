7dedf80953acbb067c0b8daa2a6703f92da6ecd5
Pascal S. de Kloe <pascal@quies.net>
2020-08-22 17:35:20 +0200

    Preserve nanoseconds on events in PostgreSQL.

--- db/migrations/20200818151607-events.sql
+++ db/migrations/20200818151607-events.sql
@@ -5,34 +5,34 @@ CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
 CREATE TABLE add_events (
 	tx		CHAR(64) NOT NULL,
-	chain		VARCHAR(16) NOT NULL,
-	from_addr	VARCHAR(64) NOT NULL,
-	to_addr		VARCHAR(64) NOT NULL,
+	chain		VARCHAR(8) NOT NULL,
+	from_addr	CHAR(48) NOT NULL,
+	to_addr		CHAR(48) NOT NULL,
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	memo		TEXT NOT NULL,
 	rune_E8		BIGINT NOT NULL,
-	pool		VARCHAR(64) NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	pool		VARCHAR(32) NOT NULL,
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('add_events', 'block_timestamp');
+SELECT create_hypertable('add_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE bond_events (
 	tx		CHAR(64) NOT NULL,
-	chain		VARCHAR(16) NOT NULL,
-	from_addr	VARCHAR(64) NOT NULL,
-	to_addr		VARCHAR(64) NOT NULL,
+	chain		VARCHAR(8) NOT NULL,
+	from_addr	CHAR(48) NOT NULL,
+	to_addr		CHAR(48) NOT NULL,
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	memo		TEXT NOT NULL,
 	bound_type	VARCHAR(32) NOT NULL,
 	E8		BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('bond_events', 'block_timestamp');
+SELECT create_hypertable('bond_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE errata_events (
@@ -40,10 +40,10 @@ CREATE TABLE errata_events (
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	rune_E8		BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('errata_events', 'block_timestamp');
+SELECT create_hypertable('errata_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE fee_events (
@@ -51,10 +51,10 @@ CREATE TABLE fee_events (
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	pool_deduct	BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('fee_events', 'block_timestamp');
+SELECT create_hypertable('fee_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE gas_events (
@@ -62,147 +62,151 @@ CREATE TABLE gas_events (
 	asset_E8	BIGINT NOT NULL,
 	rune_E8		BIGINT NOT NULL,
 	tx_count	BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('gas_events', 'block_timestamp');
+SELECT create_hypertable('gas_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE outbound_events (
 	tx		CHAR(64) NOT NULL,
-	chain		VARCHAR(16) NOT NULL,
-	from_addr	VARCHAR(127) NOT NULL,
-	to_addr		VARCHAR(127) NOT NULL,
+	chain		VARCHAR(8) NOT NULL,
+	from_addr	CHAR(48) NOT NULL,
+	to_addr		CHAR(48) NOT NULL,
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	memo		TEXT NOT NULL,
 	in_tx		CHAR(64) NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('outbound_events', 'block_timestamp');
+SELECT create_hypertable('outbound_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
-CREATE INDEX ON outbound_events (in_tx);
+CREATE INDEX ON outbound_events USING HASH (in_tx);
 
 
 CREATE TABLE pool_events (
 	asset		VARCHAR(32) NOT NULL,
 	status		VARCHAR(64) NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('pool_events', 'block_timestamp');
+SELECT create_hypertable('pool_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE refund_events (
 	tx		CHAR(64) NOT NULL,
-	chain		VARCHAR(16) NOT NULL,
-	from_addr	VARCHAR(64) NOT NULL,
-	to_addr		VARCHAR(64) NOT NULL,
+	chain		VARCHAR(8) NOT NULL,
+	from_addr	CHAR(48) NOT NULL,
+	to_addr		CHAR(48) NOT NULL,
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	memo		TEXT NOT NULL,
 	code		BIGINT NOT NULL,
 	reason		TEXT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('refund_events', 'block_timestamp');
+SELECT create_hypertable('refund_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE reserve_events (
 	tx		CHAR(64) NOT NULL,
-	chain		VARCHAR(16) NOT NULL,
-	from_addr	VARCHAR(64) NOT NULL,
-	to_addr		VARCHAR(64) NOT NULL,
+	chain		VARCHAR(8) NOT NULL,
+	from_addr	CHAR(48) NOT NULL,
+	to_addr		CHAR(48) NOT NULL,
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	memo		TEXT NOT NULL,
-	addr		VARCHAR(128) NOT NULL,
+	addr		CHAR(48) NOT NULL,
 	E8		BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('reserve_events', 'block_timestamp');
+SELECT create_hypertable('reserve_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE rewards_events (
 	bond_E8		BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('rewards_events', 'block_timestamp');
+SELECT create_hypertable('rewards_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 CREATE TABLE rewards_pools (
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('rewards_pools', 'block_timestamp');
+SELECT create_hypertable('rewards_pools', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE stake_events (
 	pool		VARCHAR(32) NOT NULL,
-	asset_tx	VARCHAR(127) NOT NULL,
-	asset_chain	VARCHAR(16) NOT NULL,
+	asset_tx	VARCHAR(64) NOT NULL,
+	asset_chain	VARCHAR(8) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	stake_units	BIGINT NOT NULL,
 	rune_tx		CHAR(64) NOT NULL,
-	rune_addr	CHAR(64) NOT NULL,
+	rune_addr	CHAR(48) NOT NULL,
 	rune_E8		BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('stake_events', 'block_timestamp');
+SELECT create_hypertable('stake_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE swap_events (
 	tx		CHAR(64) NOT NULL,
-	chain		VARCHAR(16) NOT NULL,
-	from_addr	VARCHAR(64) NOT NULL,
-	to_addr		VARCHAR(64) NOT NULL,
+	chain		VARCHAR(8) NOT NULL,
+	from_addr	CHAR(48) NOT NULL,
+	to_addr		CHAR(48) NOT NULL,
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	memo		TEXT NOT NULL,
-	pool		VARCHAR(64) NOT NULL,
+	pool		VARCHAR(32) NOT NULL,
 	price_target	BIGINT NOT NULL,
 	trade_slip	BIGINT NOT NULL,
 	liq_fee		BIGINT NOT NULL,
 	liq_fee_in_rune	BIGINT NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('swap_events', 'block_timestamp');
+SELECT create_hypertable('swap_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 CREATE TABLE unstake_events (
 	tx		CHAR(64) NOT NULL,
-	chain		VARCHAR(16) NOT NULL,
-	from_addr	VARCHAR(64) NOT NULL,
-	to_addr		VARCHAR(64) NOT NULL,
+	chain		VARCHAR(8) NOT NULL,
+	from_addr	CHAR(48) NOT NULL,
+	to_addr		CHAR(48) NOT NULL,
 	asset		VARCHAR(32) NOT NULL,
 	asset_E8	BIGINT NOT NULL,
 	memo		TEXT NOT NULL,
-	pool		VARCHAR(64) NOT NULL,
+	pool		VARCHAR(32) NOT NULL,
 	stake_units	BIGINT NOT NULL,
 	basis_points	BIGINT NOT NULL,
 	asymmetry	DOUBLE PRECISION NOT NULL,
-	block_timestamp	TIMESTAMP WITHOUT TIME ZONE NOT NULL
+	block_timestamp	BIGINT NOT NULL
 );
 
-SELECT create_hypertable('unstake_events', 'block_timestamp');
+SELECT create_hypertable('unstake_events', 'block_timestamp', chunk_time_interval => 86400000000000);
 
 
 -- +migrate Down
 
 DROP TABLE add_events;
+DROP TABLE bond_events;
+DROP TABLE errata_events;
 DROP TABLE fee_events;
 DROP TABLE gas_events;
 DROP TABLE outbound_events;
 DROP TABLE pool_events;
 DROP TABLE refund_events;
 DROP TABLE reserve_events;
+DROP TABLE rewards_events;
+DROP TABLE rewards_pools;
 DROP TABLE stake_events;
 DROP TABLE swap_events;
 DROP TABLE unstake_events;
--- internal/timeseries/out.go
+++ internal/timeseries/out.go
@@ -20,7 +20,7 @@ type eventListener struct{}
 func (_ eventListener) OnAdd(e *event.Add, meta *event.Metadata) {
 	const q = `INSERT INTO add_events (tx, chain, from_addr, to_addr, asset, asset_E8, memo, rune_E8, pool, block_timestamp)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
-	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.RuneE8, e.Pool, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.RuneE8, e.Pool, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("add event lost on ", err)
 	}
@@ -29,7 +29,7 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
 func (_ eventListener) OnBond(e *event.Bond, meta *event.Metadata) {
 	const q = `INSERT INTO bond_events (tx, chain, from_addr, to_addr, asset, asset_E8, memo, bound_type, E8, block_timestamp)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
-	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.BoundType, e.E8, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.BoundType, e.E8, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("bond event lost on ", err)
 	}
@@ -38,7 +38,7 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
 func (_ eventListener) OnErrata(e *event.Errata, meta *event.Metadata) {
 	const q = `INSERT INTO errata_events (in_tx, asset, asset_E8, rune_E8, block_timestamp)
 VALUES ($1, $2, $3, $4, $5)`
-	_, err := DBExec(q, e.InTx, e.Asset, e.AssetE8, e.RuneE8, meta.BlockTimestamp)
+	_, err := DBExec(q, e.InTx, e.Asset, e.AssetE8, e.RuneE8, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("errata event lost on ", err)
 	}
@@ -47,7 +47,7 @@ VALUES ($1, $2, $3, $4, $5)`
 func (_ eventListener) OnFee(e *event.Fee, meta *event.Metadata) {
 	const q = `INSERT INTO fee_events (tx, asset, asset_E8, pool_deduct, block_timestamp)
 VALUES ($1, $2, $3, $4, $5)`
-	_, err := DBExec(q, e.Tx, e.Asset, e.AssetE8, e.PoolDeduct, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Tx, e.Asset, e.AssetE8, e.PoolDeduct, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("fee event lost on ", err)
 	}
@@ -56,7 +56,7 @@ VALUES ($1, $2, $3, $4, $5)`
 func (_ eventListener) OnGas(e *event.Gas, meta *event.Metadata) {
 	const q = `INSERT INTO gas_events (asset, asset_E8, rune_E8, tx_count, block_timestamp)
 VALUES ($1, $2, $3, $4, $5)`
-	_, err := DBExec(q, e.Asset, e.AssetE8, e.RuneE8, e.TxCount, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Asset, e.AssetE8, e.RuneE8, e.TxCount, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("gas event lost on ", err)
 	}
@@ -65,7 +65,7 @@ VALUES ($1, $2, $3, $4, $5)`
 func (_ eventListener) OnOutbound(e *event.Outbound, meta *event.Metadata) {
 	const q = `INSERT INTO outbound_events (tx, chain, from_addr, to_addr, asset, asset_E8, memo, in_tx, block_timestamp)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
-	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.InTx, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.InTx, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("outound event lost on ", err)
 	}
@@ -74,7 +74,7 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
 func (_ eventListener) OnPool(e *event.Pool, meta *event.Metadata) {
 	const q = `INSERT INTO pool_events (asset, status, block_timestamp)
 VALUES ($1, $2, $3)`
-	_, err := DBExec(q, e.Asset, e.Status, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Asset, e.Status, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("pool event lost on ", err)
 	}
@@ -83,7 +83,7 @@ VALUES ($1, $2, $3)`
 func (_ eventListener) OnRefund(e *event.Refund, meta *event.Metadata) {
 	const q = `INSERT INTO refund_events (tx, chain, from_addr, to_addr, asset, asset_E8, memo, code, reason, block_timestamp)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
-	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.Code, e.Reason, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.Code, e.Reason, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("refund event lost on ", err)
 	}
@@ -92,7 +92,7 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
 func (_ eventListener) OnReserve(e *event.Reserve, meta *event.Metadata) {
 	const q = `INSERT INTO reserve_events (tx, chain, from_addr, to_addr, asset, asset_E8, memo, addr, E8, block_timestamp)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
-	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.Addr, e.E8, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.Addr, e.E8, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("reserve event lost on ", err)
 	}
@@ -100,13 +100,13 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
 
 func (_ eventListener) OnRewards(e *event.Rewards, meta *event.Metadata) {
 	const q = "INSERT INTO rewards_events (bond_E8, block_timestamp) VALUES ($1, $2)"
-	_, err := DBExec(q, e.BondE8, meta.BlockTimestamp)
+	_, err := DBExec(q, e.BondE8, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("reserve event lost on ", err)
 	}
 	for _, a := range e.Pool {
 		const q = "INSERT INTO rewards_pools (asset, asset_E8, block_timestamp) VALUES ($1, $2, $3)"
-		_, err := DBExec(q, a.Asset, a.E8, meta.BlockTimestamp)
+		_, err := DBExec(q, a.Asset, a.E8, meta.BlockTimestamp.UnixNano())
 		if err != nil {
 			log.Print("reserve event pool lost on ", err)
 		}
@@ -120,7 +120,7 @@ func (_ eventListener) OnSlash(e *event.Slash, meta *event.Metadata) {
 func (_ eventListener) OnStake(e *event.Stake, meta *event.Metadata) {
 	const q = `INSERT INTO stake_events (pool, asset_tx, asset_chain, asset_E8, rune_tx, rune_addr, rune_E8, stake_units, block_timestamp)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
-	_, err := DBExec(q, e.Pool, e.AssetTx, e.AssetChain, e.AssetE8, e.RuneTx, e.RuneAddr, e.RuneE8, e.StakeUnits, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Pool, e.AssetTx, e.AssetChain, e.AssetE8, e.RuneTx, e.RuneAddr, e.RuneE8, e.StakeUnits, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("stake event lost on ", err)
 	}
@@ -129,7 +129,7 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
 func (_ eventListener) OnSwap(e *event.Swap, meta *event.Metadata) {
 	const q = `INSERT INTO swap_events (tx, chain, from_addr, to_addr, asset, asset_E8, memo, pool, price_target, trade_slip, liq_fee, liq_fee_in_rune, block_timestamp)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`
-	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.Pool, e.PriceTarget, e.TradeSlip, e.LiqFee, e.LiqFeeInRune, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.Pool, e.PriceTarget, e.TradeSlip, e.LiqFee, e.LiqFeeInRune, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("swap event lost on ", err)
 	}
@@ -138,7 +138,7 @@ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`
 func (_ eventListener) OnUnstake(e *event.Unstake, meta *event.Metadata) {
 	const q = `INSERT INTO unstake_events (tx, chain, from_addr, to_addr, asset, asset_E8, memo, pool, stake_units, basis_points, asymmetry, block_timestamp)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`
-	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.Pool, e.StakeUnits, e.BasisPoints, e.Asymmetry, meta.BlockTimestamp)
+	_, err := DBExec(q, e.Tx, e.Chain, e.FromAddr, e.ToAddr, e.Asset, e.AssetE8, e.Memo, e.Pool, e.StakeUnits, e.BasisPoints, e.Asymmetry, meta.BlockTimestamp.UnixNano())
 	if err != nil {
 		log.Print("unstake event lost on ", err)
 	}
