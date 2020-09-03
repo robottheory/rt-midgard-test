a7a2c0d345f4890883b4b39bbb1fcf50775e4453
manolodewiner <manolosingular@gmail.com>
2020-09-03 11:16:56 +0200

    define swap intervals

--- internal/timeseries/stat/swap.go
+++ internal/timeseries/stat/swap.go
@@ -21,7 +21,6 @@ FROM swap_events
 WHERE block_timestamp >= $1 AND block_timestamp < $2`
 
 	return querySwaps(q, w.Start.UnixNano(), w.End.UnixNano())
-
 }
 
 func PoolSwapsLookup(pool string, w Window) (PoolSwaps, error) {
@@ -32,18 +31,66 @@ FROM swap_events
 WHERE pool = $1 AND block_timestamp >= $2 AND block_timestamp < $3`
 
 	return querySwaps(q, pool, w.Start.UnixNano(), w.End.UnixNano())
-
 }
 
-func PoolSwapsFeesLookup(pool string, w Window) (PoolSwaps, error) {
+type TotalSwapQuant struct {
+	TxCount             int64
+	TotalVolume         int64   // toRuneVolume + toAssetVolume (in RUNE)
+	TotalFees           int64   // fees in RUNE: toRuneFees + toAssetFees
+	MeantoAssetSlippage float64 // buy slippage in RUNE
+	MeanToRuneSlippage  float64 // sell slippage in RUNE
+	MeanSlippage        float64 // total slippage in RUNE: buySlippage + sellSlippage
+}
+type PoolSwapsInterval struct {
+	ToAssetCount   int64 // swaps in this period from RUNE -> ASSET
+	ToRuneCount    int64 // swaps in this period from ASSET -> RUNE
+	ToAssetVolume  int64 // volume for RUNE->ASSET (in RUNE)
+	ToRuneVolume   int64 // volume for ASSET->RUNE (in RUNE)
+	ToAssetLiqFees int64 // buy fees in RUNE
+	ToRuneLiqFees  int64 // sell fees in RUNE
+	Timestamp      time.Time
+
+	TotalSwapQuant
+}
+
+func PoolSwapsIntervalLookup(pool string, interval uint64, w Window) ([]PoolSwapsInterval, error) {
 	w.normalize()
 
-	const q = `SELECT  COALESCE(AVG(price_target), 0), COALESCE(SUM(trade_slip), 0), COALESCE(SUM(liq_fee), 0), COALESCE(SUM(liq_fee_in_rune), 0), COALESCE(MIN(block_timestamp), 0), COALESCE(MAX(block_timestamp), 0)
+	const qBuy = `SELECT COALESCE(COUNT(*), 0), COALESCE(SUM(from_e8), 0), COALESCE(SUM(liq_fee_in_rune_e8), 0), time_bucket($1, block_timestamp) AS tb
 FROM swap_events
-WHERE pool = $1 AND block_timestamp >= $2 AND block_timestamp < $3`
+WHERE pool = $2 AND from_asset<>pool AND block_timestamp >= $3 AND block_timestamp < $4
+GROUP BY tb
+`
+	const qSell = `SELECT COALESCE(COUNT(*), 0), COALESCE(SUM(from_e8), 0), COALESCE(SUM(liq_fee_in_rune_e8), 0), time_bucket($1, block_timestamp) AS tb
+FROM swap_events
+WHERE pool = $2 AND from_asset=pool AND block_timestamp >= $3 AND block_timestamp < $4
+GROUP BY tb
+`
 
-	return querySwaps(q, pool, w.Start.UnixNano(), w.End.UnixNano())
+	rowsBuy, err := DBQuery(qBuy, interval, pool, w.Start.UnixNano(), w.End.UnixNano())
+	if err != nil {
+		return []PoolSwapsInterval{}, err
+	}
+	defer rowsBuy.Close()
+	rowsSell, err := DBQuery(qSell, interval, pool, w.Start.UnixNano(), w.End.UnixNano())
+	if err != nil {
+		return []PoolSwapsInterval{}, err
+	}
+	defer rowsSell.Close()
 
+	var pools []PoolSwapsInterval
+	for rowsBuy.Next() {
+		var s PoolSwapsInterval
+		if err := rowsBuy.Scan(&s.ToAssetCount, &s.ToAssetVolume, &s.ToAssetLiqFees, &s.Timestamp); err != nil {
+			return pools, err
+		}
+		if err := rowsSell.Scan(&s.ToRuneCount, &s.ToRuneVolume, &s.ToRuneLiqFees, &s.Timestamp); err != nil {
+			return pools, err
+		}
+		// TODO operations
+		pools = append(pools, s)
+	}
+	return pools, rowsBuy.Err()
 }
 
 func querySwaps(q string, args ...interface{}) (PoolSwaps, error) {
