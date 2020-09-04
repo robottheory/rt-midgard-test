87dfe7986d8348877914fcfc8c88320797aae573
Pascal S. de Kloe <pascal@quies.net>
2020-09-04 19:48:53 +0200

    Buy & sell swap lookups without buckets.

--- internal/timeseries/stat/swap.go
+++ internal/timeseries/stat/swap.go
@@ -36,12 +36,26 @@ type PoolBuySwaps struct {
 	MeanToAssetSlippage float64 // buy slippage in RUNE
 	First, Last         time.Time
 }
+
 type PoolSellSwaps struct {
 	ToRuneCount        int64 // swaps in this period from ASSET -> RUNE
 	ToRuneVolume       int64 // volume for ASSET->RUNE (in RUNE)
 	ToRuneLiqFees      int64 // sell fees in RUNE
 	MeanToRuneSlippage int64 // sell slippage in RUNE
 	First, Last        time.Time
+}
+
+func PoolBuySwapsLookup(pool string, w Window) (PoolBuySwaps, error) {
+	const q = `SELECT COALESCE(COUNT(*), 0), COALESCE(SUM(from_e8), 0), COALESCE(SUM(liq_fee_in_rune_e8), 0), COALESCE(SUM(trade_slip_bp), 0), COALESCE(MIN(block_timestamp), 0), COALESCE(MAX(block_timestamp), 0)
+	FROM swap_events
+	WHERE pool = $1 AND from_asset<>pool AND block_timestamp >= $2 AND block_timestamp < $3
+	`
+
+	swaps, err := queryBuySwaps(q, pool, w.Since.UnixNano(), w.Until.UnixNano())
+	if err != nil || len(swaps) == 0 {
+		return PoolBuySwaps{}, err
+	}
+	return swaps[0], nil
 }
 
 func PoolBuySwapsBucketsLookup(pool string, bucketSize time.Duration, w Window) ([]PoolBuySwaps, error) {
@@ -51,18 +65,23 @@ func PoolBuySwapsBucketsLookup(pool string, bucketSize time.Duration, w Window) 
 	GROUP BY time_bucket($4, block_timestamp)
 	ORDER BY time_bucket($4, block_timestamp)
 	`
-	rows, err := DBQuery(q, pool, w.Since.UnixNano(), w.Until.UnixNano(), bucketSize.Nanoseconds())
+
+	return queryBuySwaps(q, pool, w.Since.UnixNano(), w.Until.UnixNano(), bucketSize.Nanoseconds())
+}
+
+func queryBuySwaps(q string, args ...interface{}) ([]PoolBuySwaps, error) {
+	rows, err := DBQuery(q, args...)
 	if err != nil {
-		return []PoolBuySwaps{}, err
+		return nil, err
 	}
 	defer rows.Close()
 
-	var pools []PoolBuySwaps
+	var swaps []PoolBuySwaps
 	for rows.Next() {
 		var r PoolBuySwaps
 		var first, last int64
 		if err := rows.Scan(&r.ToAssetCount, &r.ToAssetVolume, &r.ToAssetLiqFees, &r.MeanToAssetSlippage, &first, &last); err != nil {
-			return pools, err
+			return swaps, err
 		}
 		if first != 0 {
 			r.First = time.Unix(0, first)
@@ -70,9 +89,22 @@ func PoolBuySwapsBucketsLookup(pool string, bucketSize time.Duration, w Window) 
 		if last != 0 {
 			r.Last = time.Unix(0, last)
 		}
-		pools = append(pools, r)
+		swaps = append(swaps, r)
 	}
-	return pools, rows.Err()
+	return swaps, rows.Err()
+}
+
+func PoolSellSwapsLookup(pool string, w Window) (PoolSellSwaps, error) {
+	const q = `SELECT COALESCE(COUNT(*), 0), COALESCE(SUM(to_e8_min), 0), COALESCE(SUM(liq_fee_in_rune_e8), 0), COALESCE(SUM(trade_slip_bp), 0), COALESCE(MIN(block_timestamp), 0), COALESCE(MAX(block_timestamp), 0)
+	FROM swap_events
+	WHERE pool = $1 AND from_asset=pool AND block_timestamp >= $2 AND block_timestamp < $3
+	`
+
+	swaps, err := querySellSwaps(q, pool, w.Since.UnixNano(), w.Until.UnixNano())
+	if err != nil || len(swaps) == 0 {
+		return PoolSellSwaps{}, err
+	}
+	return swaps[0], nil
 }
 
 func PoolSellSwapsBucketsLookup(pool string, bucketSize time.Duration, w Window) ([]PoolSellSwaps, error) {
@@ -82,18 +114,23 @@ func PoolSellSwapsBucketsLookup(pool string, bucketSize time.Duration, w Window)
 	GROUP BY time_bucket($4, block_timestamp)
 	ORDER BY time_bucket($4, block_timestamp)
 	`
-	rows, err := DBQuery(q, pool, w.Since.UnixNano(), w.Until.UnixNano(), bucketSize.Nanoseconds())
+
+	return querySellSwaps(q, pool, w.Since.UnixNano(), w.Until.UnixNano(), bucketSize.Nanoseconds())
+}
+
+func querySellSwaps(q string, args ...interface{}) ([]PoolSellSwaps, error) {
+	rows, err := DBQuery(q, args...)
 	if err != nil {
-		return []PoolSellSwaps{}, err
+		return nil, err
 	}
 	defer rows.Close()
 
-	var pools []PoolSellSwaps
+	var swaps []PoolSellSwaps
 	for rows.Next() {
 		var r PoolSellSwaps
 		var first, last int64
 		if err := rows.Scan(&r.ToRuneCount, &r.ToRuneVolume, &r.ToRuneLiqFees, &r.MeanToRuneSlippage, &first, &last); err != nil {
-			return pools, err
+			return swaps, err
 		}
 		if first != 0 {
 			r.First = time.Unix(0, first)
@@ -101,9 +138,9 @@ func PoolSellSwapsBucketsLookup(pool string, bucketSize time.Duration, w Window)
 		if last != 0 {
 			r.Last = time.Unix(0, last)
 		}
-		pools = append(pools, r)
+		swaps = append(swaps, r)
 	}
-	return pools, rows.Err()
+	return swaps, rows.Err()
 }
 
 func querySwaps(q string, args ...interface{}) (PoolSwaps, error) {
--- internal/timeseries/stat/swap_test.go
+++ internal/timeseries/stat/swap_test.go
@@ -2,6 +2,7 @@ package stat
 
 import (
 	"testing"
+	"time"
 
 	"github.com/pascaldekloe/sqltest"
 )
@@ -9,6 +10,42 @@ import (
 func TestPoolSwapsLookup(t *testing.T) {
 	DBQuery = sqltest.NewTx(t).Query
 	got, err := PoolSwapsLookup("BNB.MATIC-416", Window{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("got %+v", got)
+}
+
+func TestPoolBuySwapsLookup(t *testing.T) {
+	DBQuery = sqltest.NewTx(t).Query
+	got, err := PoolBuySwapsLookup("BNB.MATIC-416", Window{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("got %+v", got)
+}
+
+func TestPoolBuySwapsBucketsLookup(t *testing.T) {
+	DBQuery = sqltest.NewTx(t).Query
+	got, err := PoolBuySwapsBucketsLookup("BNB.MATIC-416", 5*time.Minute, Window{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("got %+v", got)
+}
+
+func TestPoolSellSwapsLookup(t *testing.T) {
+	DBQuery = sqltest.NewTx(t).Query
+	got, err := PoolSellSwapsLookup("BNB.MATIC-416", Window{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("got %+v", got)
+}
+
+func TestPoolSellSwapsBucketsLookup(t *testing.T) {
+	DBQuery = sqltest.NewTx(t).Query
+	got, err := PoolSellSwapsBucketsLookup("BNB.MATIC-416", 5*time.Minute, Window{})
 	if err != nil {
 		t.Fatal(err)
 	}
