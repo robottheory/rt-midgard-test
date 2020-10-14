33c1336aa1553118c3fb6708a819979c85784110
Csaba Andras <acsaba@gmail.com>
2020-10-14 14:34:02 +0200

    Create test database.
    Old database setup was connecting to the production database, and it
    was doing every query in a database transaction. That setup was not
    able to handle parallel queries in the code. On the long term we plan
    the tests not to connect to the production database at all, but to use
    the pgtest database.
    
    Change also introduces a new test and corrects an bug introduced in earlier
    commit (height was not set properly).

---
+++ internal/timeseries/testdb_test.go
@@ -0,0 +1,100 @@
+// Package name is timeseries_test in order to depend on internal/api without a dependency loop.
+package timeseries_test
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"gitlab.com/thorchain/midgard/internal/api"
+	"gitlab.com/thorchain/midgard/internal/timeseries"
+	"gitlab.com/thorchain/midgard/internal/timeseries/stat"
+)
+
+var testDBQuery func(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+var testDBExec func(query string, args ...interface{}) (sql.Result, error)
+
+func init() {
+	// TODO(acsaba): Should have special handling for short mode?
+	testDbPort := 5433
+	db, err := sql.Open("pgx", fmt.Sprintf("user=midgard dbname=midgard sslmode=disable password=password host=localhost port=%d", testDbPort))
+	if err != nil {
+		log.Fatal("Failed to connect to PostgreSQL. Did you `docker-compose up -d pg`? (", err, ")")
+	}
+
+	testDBQuery = db.QueryContext
+	testDBExec = db.Exec
+}
+
+func setupTestDB() {
+	stat.DBQuery = testDBQuery
+	timeseries.DBExec = testDBExec
+	timeseries.DBQuery = testDBQuery
+}
+
+func toTime(s string) time.Time {
+	const format = "2006-01-02 15:04:05"
+	t, err := time.Parse(format, s)
+	if err != nil {
+		log.Panicf("Bad date format %v", err)
+	}
+	return t
+}
+
+// Execute a query on the database.
+func mustExec(t *testing.T, query string, args ...interface{}) {
+	_, err := timeseries.DBExec(query, args...)
+	if err != nil {
+		t.Fatal("db query failed. Did you `docker-compose up -d pg`? ", err, "query: ", query, "args: ", args)
+	}
+}
+
+// Make an HTTP call to the /v1 api, return the body which can be parsed as a JSON.
+func callV1(t *testing.T, url string) (body []byte) {
+	api.InitHandler("", []string{})
+	req := httptest.NewRequest("GET", url, nil)
+	w := httptest.NewRecorder()
+	api.Handler.ServeHTTP(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal("error reading body:", res.Body)
+	}
+
+	if res.Status != "200 OK" {
+		t.Fatal("Bad response status:", res.Status, ". Body: ", string(body))
+	}
+
+	return body
+}
+
+type fakeStake struct {
+	assetChain     string
+	blockTimestamp int64
+}
+
+func insertStakeEvent(t *testing.T, fake fakeStake) {
+	if fake.assetChain == "" {
+		fake.assetChain = "BNB.BNB"
+	}
+
+	const insertq = (`INSERT INTO stake_events ` +
+		`(pool, asset_tx, asset_chain, asset_E8, rune_tx, rune_addr, rune_E8, stake_units, block_timestamp) ` +
+		`VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`)
+
+	mustExec(t, insertq, fake.assetChain, "asset_tx", "chain", 1, "rune_tx", "rune_addr", 2, 3, fake.blockTimestamp)
+}
+
+func insertBlockLog(t *testing.T, height, timestamp int64) {
+	const insertq = (`INSERT INTO block_log ` +
+		`(height, timestamp, hash) ` +
+		`VALUES ($1, $2, $3)`)
+
+	mustExec(t, insertq, height, timestamp, fmt.Sprintf("%d-%d", height, timestamp))
+}
