268776fc6a160cd423d334d995e1210d6feb0590
Csaba Andras <acsaba@gmail.com>
2020-12-04 21:07:05 +0100

    Refactor buckets logic, no functionality change.

--- internal/db/buckets.go
+++ internal/db/buckets.go
@@ -7,7 +7,6 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
-	"time"
 )
 
 // Window specifies the applicable time period.
@@ -57,78 +56,28 @@ var DBIntervalName = []string{
 	Year:    "year",
 }
 
-var intervalFromJSONParamMap = map[string]Interval{
-	"5min":    Min5,
-	"hour":    Hour,
-	"day":     Day,
-	"week":    Week,
-	"month":   Month,
-	"quarter": Quarter,
-	"year":    Year,
+const maxIntervalCount = 100
+
+// Used for extending bounds at least to the next occurance.
+var maxDuration = map[Interval]Second{
+	Min5:    60 * 5,
+	Hour:    60 * 60,
+	Day:     60 * 60 * 24,
+	Week:    60 * 60 * 24 * 7,
+	Month:   60 * 60 * 24 * 31,
+	Quarter: 60 * 60 * 24 * 31 * 3,
+	Year:    60 * 60 * 24 * 31 * 12,
 }
 
-func IntervalFromJSONParam(param string) (Interval, error) {
-	ret, ok := intervalFromJSONParamMap[strings.ToLower(param)]
-	if !ok {
-		return UndefinedInterval, errors.New("Invalid interval (" + param +
-			"), accepted values: 5min, hour, day, week, month, quarter, year")
-	}
-	return ret, nil
-}
-
-const maxIntervalCount = 101
-
-// We want to limit the respons intervals, but we want to restrict the
-// Database lookup range too so we don't do all the work unnecessarily.
-var maxDuration = map[Interval]time.Duration{
-	Min5:    time.Minute * 5 * maxIntervalCount,
-	Hour:    time.Hour * maxIntervalCount,
-	Day:     time.Hour * 24 * maxIntervalCount,
-	Week:    time.Hour * 24 * 7 * maxIntervalCount,
-	Month:   time.Hour * 24 * 31 * maxIntervalCount,
-	Quarter: time.Hour * 24 * 31 * 3 * maxIntervalCount,
-	Year:    time.Hour * 24 * 365 * maxIntervalCount,
-}
-
-// A reasonable period for gapfil which guaranties that date_trunc will
-// create all the needed entries.
-var reasonableGapfillParam = map[Interval]string{
-	Min5:    "300::BIGINT",      // 5 minutes
-	Hour:    "3600::BIGINT",     // 1 hour
-	Day:     "86400::BIGINT",    // 24 hours
-	Week:    "604800::BIGINT",   // 7 days
-	Month:   "2160000::BIGINT",  // 25 days
-	Quarter: "7344000::BIGINT",  // 85 days
-	Year:    "25920000::BIGINT", // 300 days
-}
-
-// In addition of setting sane default values it also restricts window length.
-// TODO(acsaba): filling default seems not to be used, delete,
-//     keep only setting max duration.
-func fillMissingFromTo(w Window, inv Interval) Window {
-	max := maxDuration[inv]
-
-	if w.From != 0 && w.Until == 0 {
-		// if only since is defined
-		limitedTime := w.From.Add(max)
-		w.Until = limitedTime
-	} else if w.From == 0 && w.Until != 0 {
-		// if only until is defined
-		limitedTime := w.Until.Add(-max)
-		w.From = limitedTime
-	} else if w.From == 0 && w.Until == 0 {
-		// if neither is defined
-		w.Until = TimeToSecond(time.Now())
-	}
-
-	// if the starting time lies outside the limit
-	limitedTime := w.Until.Add(-max)
-	if w.From < limitedTime {
-		// limit the value
-		w.From = limitedTime
-	}
-
-	return w
+// Used for gapfill, to make sure every interval has one value.
+var minDuration = map[Interval]Second{
+	Min5:    60 * 5,
+	Hour:    60 * 60,
+	Day:     60 * 60 * 24,
+	Week:    60 * 60 * 24 * 7,
+	Month:   60 * 60 * 24 * 28,
+	Quarter: 60 * 60 * 24 * 28 * 3,
+	Year:    60 * 60 * 24 * 28 * 12,
 }
 
 // Returns all the buckets for the window, so other queries don't have to care about gapfill functionality.
@@ -138,25 +87,22 @@ func generateBuckets(ctx context.Context, interval Interval, w Window) (Seconds,
 	// in order not to actually select any data.
 	// We could consider writing an sql function instead or programming dategeneration in go.
 
-	w = fillMissingFromTo(w, interval)
-	gapfill := reasonableGapfillParam[interval]
-
-	q := fmt.Sprintf(`
+	q := `
 		WITH gapfill AS (
 			SELECT
-				time_bucket_gapfill(%s, block_timestamp, $1::BIGINT, $2::BIGINT) as bucket
+				time_bucket_gapfill($1::BIGINT, block_timestamp, $2::BIGINT, $3::BIGINT) as bucket
 			FROM block_pool_depths
 			WHERE 1=0
 			GROUP BY bucket)
 		SELECT
 			EXTRACT(EPOCH FROM
-				date_trunc($3, to_timestamp(bucket/300*300)))::BIGINT as truncated
+				date_trunc($4, to_timestamp(bucket/300*300)))::BIGINT as truncated
 		FROM gapfill
 		GROUP BY truncated
 		ORDER BY truncated ASC
-	`, gapfill)
+	`
 
-	rows, err := Query(ctx, q, w.From, w.Until-1, DBIntervalName[interval])
+	rows, err := Query(ctx, q, minDuration[interval], w.From, w.Until-1, DBIntervalName[interval])
 	if err != nil {
 		return nil, w, err
 	}
@@ -199,6 +145,16 @@ func BucketsFromWindow(ctx context.Context, window Window, interval Interval) (r
 	return
 }
 
+var intervalFromJSONParamMap = map[string]Interval{
+	"5min":    Min5,
+	"hour":    Hour,
+	"day":     Day,
+	"week":    Week,
+	"month":   Month,
+	"quarter": Quarter,
+	"year":    Year,
+}
+
 // TODO(acsaba): differentiate between user error and server error.
 func BucketsFromQuery(ctx context.Context, query url.Values) (ret Buckets, err error) {
 	from, err := convertStringToTime(query.Get("from"))
@@ -211,13 +167,20 @@ func BucketsFromQuery(ctx context.Context, query url.Values) (ret Buckets, err e
 		err = fmt.Errorf("Invalid query parameter: to (%v)", err)
 		return
 	}
+
 	intervalStr := query.Get("interval")
 	if intervalStr == "" {
 		err = fmt.Errorf("'interval' parameter is required")
 		return
 	}
+	interval, ok := intervalFromJSONParamMap[strings.ToLower(intervalStr)]
+	if !ok {
+		err = fmt.Errorf(
+			"Invalid interval (%s), accepted values: 5min, hour, day, week, month, quarter, year",
+			intervalStr)
+		return
+	}
 
-	interval, err := IntervalFromJSONParam(intervalStr)
 	origWindow := Window{
 		From:  from,
 		Until: to,
