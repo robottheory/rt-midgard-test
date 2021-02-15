b7fb5017b3fa0e9aa6d108ab6859001940bac2d5
Federico Rodriguez <federodriguez.88@gmail.com>
2021-02-15 11:41:31 -0300

    Update websockets notlinux interface

--- internal/websockets/websockets_notlinux.go
+++ internal/websockets/websockets_notlinux.go
@@ -7,14 +7,25 @@ import (
 	"log"
 	"net/http"
 	"runtime"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 // NOTE: Websockes uses linux only syscalls so currently it is not supported for other os.
 // For local dev on windows/macos you may need to run the application using Docker
-func Serve(connectionLimit int) {
+
+type Quit struct {
+	quitTriggered chan struct{}
+	quitFinished  chan struct{}
+}
+
+func Start(connectionLimit int) *Quit {
 	log.Printf("WARNING: Websockets not implemented for os %s. Only linux is supported", runtime.GOOS)
+	return &Quit{}
+}
+
+func (q *Quit) Quit(timeout time.Duration) {
 }
 
 func WsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
