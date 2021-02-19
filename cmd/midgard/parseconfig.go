d26c7533248859954e79ffb3e6ed44a23529f9ca
Csaba Andras <acsaba@gmail.com>
2021-02-19 14:12:54 +0100

    Move config to a separate file.

---
+++ cmd/midgard/parseconfig.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+	"time"
+
+	"gitlab.com/thorchain/midgard/internal/db"
+)
+
+func ReadConfig() Config {
+	var ret Config
+	switch len(os.Args) {
+	case 1:
+		break // refer to defaults
+	case 2:
+		ret = *MustLoadConfigFile(os.Args[1])
+	default:
+		log.Fatal("One optional configuration file argument only—no flags")
+	}
+	return ret
+}
+
+type Config struct {
+	ListenPort      int      `json:"listen_port" split_words:"true"`
+	ShutdownTimeout Duration `json:"shutdown_timeout" split_words:"true"`
+	ReadTimeout     Duration `json:"read_timeout" split_words:"true"`
+	WriteTimeout    Duration `json:"write_timeout" split_words:"true"`
+
+	// Only for development.
+	FailOnError bool `json:"fail_on_error" split_words:"true"`
+
+	TimeScale db.Config `json:"timescale"`
+
+	ThorChain struct {
+		TendermintURL               string   `json:"tendermint_url" split_words:"true"`
+		ThorNodeURL                 string   `json:"thornode_url" split_words:"true"`
+		ReadTimeout                 Duration `json:"read_timeout" split_words:"true"`
+		LastChainBackoff            Duration `json:"last_chain_backoff" split_words:"true"`
+		ProxiedWhitelistedEndpoints []string `json:"proxied_whitelisted_endpoints" split_words:"true"`
+	} `json:"thorchain"`
+
+	Websockets struct {
+		Enable          bool `json:"enable" split_words:"true"`
+		ConnectionLimit int  `json:"connection_limit" split_words:"true"`
+	} `json:"websockets"`
+}
+
+type Duration time.Duration
+
+func (d Duration) WithDefault(def time.Duration) time.Duration {
+	if d == 0 {
+		return def
+	}
+	return time.Duration(d)
+}
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Duration(d).String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case string:
+		v, err := time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		*d = Duration(v)
+	default:
+		return errors.New("duration not a string")
+	}
+	return nil
+}
+
+func MustLoadConfigFile(path string) *Config {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal("exit on configuration file unavailable: ", err)
+	}
+	defer f.Close()
+
+	dec := json.NewDecoder(f)
+
+	// prevent config not used due typos
+	dec.DisallowUnknownFields()
+
+	var c Config
+	if err := dec.Decode(&c); err != nil {
+		log.Fatal("exit on malformed configuration: ", err)
+	}
+	return &c
+}
