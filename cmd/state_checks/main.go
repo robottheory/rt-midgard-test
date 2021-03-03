12bbb7cfab101097c73d33ac11e57a6de1c84041
Csaba Andras <acsaba@gmail.com>
2021-03-03 10:36:56 +0100

    Binary search first divergence in depths.

--- cmd/state_checks/main.go
+++ cmd/state_checks/main.go
@@ -22,10 +22,15 @@ import (
 )
 
 type Pool struct {
-	RuneDepth  int64  `json:"balance_rune,string"`
-	AssetDepth int64  `json:"balance_asset,string"`
-	Status     string `json:"status"`
 	Pool       string `json:"asset"`
+	AssetDepth int64  `json:"balance_asset,string"`
+	RuneDepth  int64  `json:"balance_rune,string"`
+	Status     string `json:"status"`
+	Timestamp  db.Nano
+}
+
+func (pool Pool) String() string {
+	return fmt.Sprintf("%s [Asset: %d, Rune: %d]", pool.Pool, pool.AssetDepth, pool.RuneDepth)
 }
 
 type State struct {
@@ -39,7 +44,8 @@ type Node struct {
 
 func main() {
 	logrus.SetFormatter(&logrus.TextFormatter{TimestampFormat: "2006-01-02 15:04:05", FullTimestamp: true})
-	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetLevel(logrus.InfoLevel)
+	// logrus.SetLevel(logrus.DebugLevel)
 
 	// TODO(acsaba): move config parsing into a separate lib.
 	// read configuration file
@@ -66,7 +72,12 @@ func main() {
 
 	thornodeState := getThornodeState(ctx, c.ThorChain.ThorNodeURL, lastHeight, lastTimestamp)
 
-	compareStates(midgardState, thornodeState)
+	poolsWithMismatchingDepths := compareStates(midgardState, thornodeState)
+
+	// binarySearch(ctx, c.ThorChain.ThorNodeURL, "ETH.ETH", 1, lastHeight)
+	for _, pool := range poolsWithMismatchingDepths {
+		binarySearch(ctx, c.ThorChain.ThorNodeURL, pool, 1, lastHeight)
+	}
 }
 
 type Config struct {
@@ -151,7 +162,7 @@ func getMidgardState(ctx context.Context, height int64, timestamp db.Nano) (stat
 	return
 }
 
-func queryThornode(thorNodeUrl string, urlPath string, height int64, dest interface{}) {
+func queryThorNode(thorNodeUrl string, urlPath string, height int64, dest interface{}) {
 	url := thorNodeUrl + urlPath + "?height=" + strconv.FormatInt(height, 10)
 	logrus.Debug("Querying thornode: ", url)
 	resp, err := http.Get(url)
@@ -168,20 +179,20 @@ func queryThornode(thorNodeUrl string, urlPath string, height int64, dest interf
 }
 
 func getThornodeState(ctx context.Context, thorNodeUrl string, height int64, timestamp db.Nano) (state State) {
-	logrus.Infof("Getting ThorNode data...")
+	logrus.Debug("Getting ThorNode data...")
 
 	var (
 		pools []Pool
 		nodes []Node
 	)
 
-	queryThornode(thorNodeUrl, "/pools", height, &pools)
+	queryThorNode(thorNodeUrl, "/pools", height, &pools)
 	state.Pools = map[string]Pool{}
 	for _, pool := range pools {
 		state.Pools[pool.Pool] = pool
 	}
 
-	queryThornode(thorNodeUrl, "/nodes", height, &nodes)
+	queryThorNode(thorNodeUrl, "/nodes", height, &nodes)
 	for _, node := range nodes {
 		if node.Status == "active" {
 			state.ActiveNodeCount++
@@ -190,7 +201,9 @@ func getThornodeState(ctx context.Context, thorNodeUrl string, height int64, tim
 	return
 }
 
-func compareStates(midgardState, thornodeState State) {
+func compareStates(midgardState, thornodeState State) (poolsWithMismatchingDepths []string) {
+	mismatchingPools := map[string]bool{}
+
 	errors := bytes.Buffer{}
 
 	for _, thornodePool := range thornodeState.Pools {
@@ -203,6 +216,7 @@ func compareStates(midgardState, thornodeState State) {
 		}
 
 		if midgardPool.RuneDepth != thornodePool.RuneDepth {
+			mismatchingPools[thornodePool.Pool] = true
 			fmt.Fprintf(
 				&errors, "%s RUNE Depth mismatch Thornode: %d, Midgard: %d\n",
 				prompt, thornodePool.RuneDepth, midgardPool.RuneDepth)
@@ -210,6 +224,7 @@ func compareStates(midgardState, thornodeState State) {
 		}
 
 		if midgardPool.AssetDepth != thornodePool.AssetDepth {
+			mismatchingPools[thornodePool.Pool] = true
 			fmt.Fprintf(
 				&errors, "%s Asset Depth mismatch Thornode: %d, Midgard: %d\n",
 				prompt, thornodePool.AssetDepth, midgardPool.AssetDepth)
@@ -238,4 +253,73 @@ func compareStates(midgardState, thornodeState State) {
 	} else {
 		logrus.Infof("All state checks OK")
 	}
+
+	for pool := range mismatchingPools {
+		poolsWithMismatchingDepths = append(poolsWithMismatchingDepths, pool)
+	}
+	return
+}
+
+func midgardPoolAtHight(ctx context.Context, pool string, height int64) Pool {
+	logrus.Debug("Gettting Midgard data at height: ", height)
+
+	q := `
+	SELECT block_timestamp, asset_e8, rune_e8
+	FROM block_pool_depths
+	WHERE
+		block_timestamp < (SELECT timestamp FROM block_log WHERE height=$1)
+		AND pool = $2
+	ORDER BY block_timestamp DESC
+	LIMIT 1
+	`
+	rows, err := db.Query(ctx, q, height, pool)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer rows.Close()
+
+	ret := Pool{Pool: pool}
+	if rows.Next() {
+		err := rows.Scan(&ret.Timestamp, &ret.AssetDepth, &ret.RuneDepth)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+	}
+
+	return ret
+}
+
+// Looks up the first difference in the (min, max) range. May return max.
+func binarySearch(ctx context.Context, thorNodeUrl string, pool string, minHeight, maxHeight int64) {
+	logrus.Infof("[%s] Binary searching in range [%d, %d)", pool, minHeight, maxHeight)
+
+	for 1 < maxHeight-minHeight {
+		middleHeight := (minHeight + maxHeight) / 2
+		logrus.Debugf(
+			"--- [%s] Binary search step [%d, %d] height: %d",
+			pool, minHeight, maxHeight, middleHeight)
+		var thorNodePool Pool
+		queryThorNode(thorNodeUrl, "/pool/"+pool, middleHeight, &thorNodePool)
+		logrus.Debug("Thornode: ", thorNodePool)
+		midgardPool := midgardPoolAtHight(ctx, pool, middleHeight)
+		logrus.Debug("Midgard: ", midgardPool)
+		ok := thorNodePool.AssetDepth == midgardPool.AssetDepth && thorNodePool.RuneDepth == midgardPool.RuneDepth
+		if ok {
+			logrus.Debug("Same at height ", middleHeight)
+			minHeight = middleHeight
+		} else {
+			logrus.Debug("Differ at height ", middleHeight)
+			maxHeight = middleHeight
+		}
+	}
+
+	logrus.Infof("[%s] First differenct at height: %d", pool, maxHeight)
+	var thorNodePool Pool
+	queryThorNode(thorNodeUrl, "/pool/"+pool, maxHeight, &thorNodePool)
+	midgardPool := midgardPoolAtHight(ctx, pool, maxHeight)
+	logrus.Info("Thornode: ", thorNodePool)
+	logrus.Info("Midgard:  ", midgardPool)
+
+	logrus.Info("Midgard Asset excess:  ", midgardPool.AssetDepth-thorNodePool.AssetDepth)
+	logrus.Info("Midgard Rune excess:   ", midgardPool.RuneDepth-thorNodePool.RuneDepth)
 }
