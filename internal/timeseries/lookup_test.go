b381da807a56ef4797268e656d1c496bbb90a885
Muninn <muninn.tc@protonmail.com>
2021-07-08 10:09:40 +0200

    Add a simple SynthUnits calculation test

--- internal/timeseries/lookup_test.go
+++ internal/timeseries/lookup_test.go
@@ -7,7 +7,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetSinglePoolSynthUnits(t *testing.T) {
+func TestSynthUnitsBigNum(t *testing.T) {
 	synthUnits := GetSinglePoolSynthUnits(context.Background(), 1878459169, 1909971564, 35168185551)
 	require.Equal(t, int64(36368256684), synthUnits)
+}
+
+func TestGetSinglePoolSynthUnits(t *testing.T) {
+	// 10% of the pool is owned by synth holders
+	synthUnits := GetSinglePoolSynthUnits(context.Background(), 10000, 2000, 90)
+	require.Equal(t, int64(10), synthUnits)
 }
