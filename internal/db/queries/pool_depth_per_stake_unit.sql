2cc476ca243f0dc8e4470598f0a204b353742464
Leif the Lucky <leiftheluckyone@gmail.com>
2021-09-23 16:39:02 -0400

    fix various major issues with pool depth per stake unit

--- internal/db/queries/pool_depth_per_stake_unit.sql
+++ internal/db/queries/pool_depth_per_stake_unit.sql
@@ -1,52 +1,78 @@
-select * from (
-    select
+-- View of the union of stake and unstake events.
+with stake_unstake_events as (
+	select
 		pool,
-		week_timestamp,
-		added_asset_e8,
-		added_rune_e8,
-		added_stake,
-		withdrawn_asset_e8,
-		withdrawn_rune_e8,
-		withdrawn_stake,
-		sum(coalesce(added_stake, 0) - coalesce(withdrawn_stake, 0)) over wnd as total_stake
-	from (
+		block_timestamp,
+		coalesce(rune_addr, asset_addr) as member_addr,
+		asset_addr,
+		rune_e8 as added_rune_e8,
+		asset_e8 as added_asset_e8,
+		stake_units as added_stake,
+		cast(NULL as BigInt) as withdrawn_rune_e8,
+		cast(NULL as BigInt) as withdrawn_asset_e8,
+		cast(NULL as BigInt) as withdrawn_stake,
+		cast(NULL as BigInt) as withdrawn_basis_points
+	from midgard.stake_events
+	union (
 		select
 			pool,
-			week_timestamp,
-			sum(added_asset_e8) as added_asset_e8,
-			sum(added_rune_e8) as added_rune_e8,
-			sum(added_stake) as added_stake,
-			sum(withdrawn_asset_e8) as withdrawn_asset_e8,
-			sum(withdrawn_rune_e8) as withdrawn_rune_e8,
-			sum(withdrawn_stake) as withdrawn_stake
-		from (
-			select
-				coalesce(stake.pool, unstake.pool) as pool,
-				coalesce(stake.block_timestamp,
-						 unstake.block_timestamp) / 1000000000 / 7 as week_timestamp,
-				stake.asset_e8 as added_asset_e8,
-				stake.rune_e8 as added_rune_e8,
-				stake.stake_units as added_stake,
-				unstake.stake_units as withdrawn_stake,
-				unstake.emit_asset_e8 as withdrawn_asset_e8,
-				unstake.emit_rune_e8 as withdrawn_rune_e8
-			from midgard.stake_events as stake full outer join
-			   (select * from midgard.unstake_events
-				where pool='ETH.ETH') as unstake
-			on stake.block_timestamp = unstake.block_timestamp
-			where stake.pool='ETH.ETH'
-			order by pool, coalesce(stake.block_timestamp, unstake.block_timestamp) asc) as timeseries
-		group by pool, week_timestamp
-		order by week_timestamp) as rolled_up
-	window wnd as (partition by pool order by week_timestamp asc)) as stake_units
-left outer join (
-	select pool,
-	    last_value(asset_e8) over wnd as depth_asset_e8,
-	    last_value(rune_e8) over wnd as depth_rune_e8,
-	    last_value(block_timestamp / 1000000000 / 7) over wnd as week_timestamp
-	from midgard.block_pool_depths
-    where pool='ETH.ETH'
-    window wnd as (partition by block_timestamp / 1000000000 / 7
-				   order by block_timestamp)) as pool_depths
-on stake_units.pool = pool_depths.pool
-    and stake_units.week_timestamp = pool_depths.week_timestamp
+			block_timestamp,
+			from_addr as member_addr,
+			cast(NULL as text) as asset_addr,
+			cast(NULL as BigInt) as added_rune_e8,
+			cast(NULL as BigInt) as added_asset_e8,
+			cast(NULL as BigInt) as added_stake,
+			emit_rune_e8 as withdrawn_rune_e8,
+			emit_asset_e8 as withdrawn_asset_e8,
+			stake_units as withdrawn_stake,
+			basis_points as withdrawn_basis_points
+		from midgard.unstake_events
+		order by block_timestamp
+	)
+),
+-- View of stake and unstake events, together with pool depths.
+stakes_and_depths as (
+	select 
+		*,
+		sum(coalesce(added_stake, 0)) over wnd - sum(coalesce(withdrawn_stake, 0)) over wnd as total_stake,
+		depth_asset_e8::numeric * depth_rune_e8 as depth_product
+	from stake_unstake_events
+	left outer join (
+		select pool,
+			asset_e8 as depth_asset_e8,
+			rune_e8 as depth_rune_e8,
+			block_timestamp
+		from midgard.block_pool_depths
+	) as depths
+	using (pool, block_timestamp)
+	where pool = 'BNB.BNB'
+	window wnd as (partition by pool order by block_timestamp)
+	order by block_timestamp
+),
+daily_stakes_and_depths as (
+	select
+		pool,
+		block_timestamp,
+		total_stake,
+		depth_product,
+		row_number() over (partition by pool, block_timestamp / 1000000000 / 60 / 60 / 24
+						   order by block_timestamp desc) as r
+	from stakes_and_depths
+),
+weekly_stakes_and_depths as (
+	select
+		pool,
+		block_timestamp,
+		total_stake,
+		depth_product,
+		row_number() over (partition by pool, block_timestamp / 1000000000 / 60 / 60 / 24 / 7
+						   order by block_timestamp desc) as r
+	from stakes_and_depths
+)
+select pool,
+	to_timestamp(block_timestamp / 1000000000)::date as date,
+	depth_product / total_stake / total_stake as depth_product_per_stake2,
+	depth_product / total_stake / total_stake < lag(depth_product / total_stake / total_stake, 1)
+		over (partition by pool order by block_timestamp) as decrease
+from daily_stakes_and_depths
+where r = 1
