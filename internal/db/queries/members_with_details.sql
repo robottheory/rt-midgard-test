0b9433db4ef466537ce5d4a61bed0653473a4a1d
Leif the Lucky <leiftheluckyone@gmail.com>
2021-12-03 19:46:23 +0100

    compute various return metrics

--- internal/db/queries/members_with_details.sql
+++ internal/db/queries/members_with_details.sql
@@ -66,7 +66,8 @@ left outer join midgard.block_pool_depths as pd using (block_timestamp, pool)
 left outer join (
 	select * from midgard.block_pool_depths
 	where pool='BNB.BUSD-BD1'
-) as usd using (block_timestamp)),
+) as usd using (block_timestamp)
+where pd.asset_e8 != 0 and usd.asset_e8 != 0),
 
 -- Aggregate added and withdrawn liquidity for each member with mark-to-market valuations.
 aggregated_members as (select
@@ -91,6 +92,7 @@ aggregated_members as (select
 		min(block_timestamp) filter (where added_stake > 0) as min_add_timestamp,
 		max(block_timestamp) filter (where added_stake > 0) as max_add_timestamp
 from events_with_partition_and_pool_depth_and_prices
+where asset_to_rune_e8 != 0 and usd_to_rune_e8 != 0
 group by pool, member_addr, asset_addr_partition
 order by pool, asset_addr_partition),
 
@@ -121,21 +123,80 @@ window wnd as (partition by pool, member_addr order by asset_addr_partition
 
 -- Select the pool depths at the latest block timestamp available for each pool.
 last_pool_depths as (select
-	* from (select
+	pool,
+	rune_depth_e8,
+	asset_depth_e8,
+	last_asset_to_rune_e8,
+	latest_pool_depth_block_timestamp
+from (select
 		pool,
 		rune_e8 as rune_depth_e8,
 		asset_e8 as asset_depth_e8,
 		cast(rune_e8 as decimal) / asset_e8 as last_asset_to_rune_e8,
+	  	block_timestamp as latest_pool_depth_block_timestamp,
 		row_number() over (partition by pool
 						   order by block_timestamp desc) as r
-	from block_pool_depths) as sequenced
+	from block_pool_depths
+	where asset_e8 != 0) as sequenced
 where r = 1),
 
-member_details_with_m2m as (
-	select * from member_details
-	full outer join last_pool_depths
-	using (pool)
+last_total_stake as (select
+		pool,
+		latest_pool_stake_block_timestamp,
+		total_stake
+	from (select
+			pool,
+			block_timestamp as latest_pool_stake_block_timestamp,
+			sum(coalesce(added_stake, 0) - coalesce(withdrawn_stake, 0)) over (partition by pool order by block_timestamp) as total_stake,
+			row_number() over (partition by pool
+							   order by block_timestamp desc) as r
+		  from stake_unstake_events) as sequenced
+where r = 1),
+
+member_details_with_latest_pool_info as (select
+	* from member_details
+	full outer join last_total_stake using (pool)
+	full outer join last_pool_depths using (pool)
+	full outer join (
+		select
+			rune_e8::decimal / asset_e8 as usd_to_rune_e8,
+			block_timestamp as usd_to_rune_block_timestamp
+		from block_pool_depths
+		where pool='BNB.BUSD-BD1') as pd
+		on usd_to_rune_block_timestamp = latest_pool_depth_block_timestamp
+),
+
+metrics as (select
+	*,
+	stake::decimal / total_stake * asset_depth_e8 as redeamable_asset_e8,
+	stake::decimal / total_stake * rune_depth_e8 as redeamable_rune_e8
+from member_details_with_latest_pool_info
+where total_stake != 0),
+
+metrics2 as (select
+	*,
+	--redeamable_asset_e8::decimal / asset_depth_e8 * rune_depth_e8 as redeamable_asset_in_rune_e8,
+	redeamable_asset_e8::decimal / asset_depth_e8 * rune_depth_e8 / usd_to_rune_e8 as redeamable_asset_in_usd_e8,
+	redeamable_rune_e8::decimal / usd_to_rune_e8 as redeamable_rune_in_usd_e8
+	from metrics
+),
+
+metrics3 as (select
+	*,
+	redeamable_asset_e8 + withdrawn_asset_e8 - added_asset_e8 as return_asset_e8,
+	redeamable_rune_e8 + withdrawn_rune_e8 - added_rune_e8 as return_rune_e8,
+	redeamable_asset_in_usd_e8 + m2m_withdrawn_asset_in_usd_e8 - m2m_added_asset_in_usd_e8 as return_asset_in_usd_e8,
+	redeamable_rune_in_usd_e8 + m2m_withdrawn_rune_in_usd_e8 - m2m_added_rune_in_usd_e8 as return_rune_in_usd_e8
+	from metrics2
+),
+
+metrics4 as (select
+	*,
+	return_asset_in_usd_e8 + return_rune_in_usd_e8 as return_usd_e8,
+	(return_asset_in_usd_e8 + return_rune_in_usd_e8) / (m2m_added_asset_in_usd_e8::bigint + m2m_added_rune_in_usd_e8) as return_usd_pct
+	from metrics3
 )
-select * from member_details_with_m2m
+
+select * from metrics4
 order by stake desc
 limit 1000
