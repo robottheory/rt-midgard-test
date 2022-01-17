e599c74757745e9e27dc8ec7a0edccddad049285
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2022-01-17 16:06:12 +0330

    update default config

--- internal/fetch/sync/blockstore/blockstore.go
+++ internal/fetch/sync/blockstore/blockstore.go
@@ -266,19 +266,7 @@ func (b *BlockStore) readTrunkHashes() []trunk {
 	return trunks
 }
 
-// TODO(freki) invalidate cache if cache is introduced
-func (b *BlockStore) cleanUp() {
-	res, err := b.getResources()
-	if err != nil {
-		log.Fatal().Err(err)
-	}
-	for _, r := range res {
-		if _, err := r.toHeight(); err != nil {
-			path := r.path(b)
-			log.Info().Msgf("BlockStore: cleanup, removing %s", path)
-			if err := os.Remove(path); err != nil {
-				log.Fatal().Err(err).Msgf("Error cleanin up resource  %s", path)
-			}
-		}
-	}
+func (b *BlockStore) getTrunkHashesPath() string {
+	// TODO(munnin): replace chain_id with configurable first hash id of the chain (chaos/stage)
+	return "./resources/hashes/chain_id"
 }
