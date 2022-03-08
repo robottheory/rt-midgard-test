4e8d26e3bbdea46f45901c6225cc38986bc09521
Muninn <muninn.tc@protonmail.com>
2022-03-08 09:27:09 +0100

    Log error when fetching remote fails

--- internal/fetch/sync/blockstore/remote.go
+++ internal/fetch/sync/blockstore/remote.go
@@ -36,10 +36,10 @@ func (b *BlockStore) updateFromRemote(ctx context.Context) {
 		log.Info().Msgf("BlockStore:  [%.2f%%] fetching chunk: %v", 100*float32(i)/n, chunkHash.name)
 		if err := b.fetchChunk(chunkHash); err != nil {
 			if err == io.EOF {
-				log.Info().Msgf("BlockStore: chunk not found %v", chunkHash)
+				log.Error().Msgf("BlockStore: chunk not found %v", chunkHash)
 				break
 			}
-			log.Warn().Err(err).Msgf("BlockStore: error updating from remote")
+			log.Error().Err(err).Msgf("BlockStore: error updating from remote")
 			return
 		}
 	}
