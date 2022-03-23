d35df8f7edf53228b4bcf826e6b105f25f1440ab
Leif the Lucky <leiftheluckyone@gmail.com>
2022-03-23 09:55:56 +0000

    Add USD return values

--- docs/earnings/src/model/model.js
+++ docs/earnings/src/model/model.js
@@ -19,27 +19,39 @@ class LPLiquidity {
 
     constructor() {
     }
-    update(memberDetails, poolDetails, actions, assetPriceInRuneByTime) {
+    update(memberDetails, poolDetails, actions, assetPriceInRuneByTime, assetPriceInUsdByTime) {
         this.pool = memberDetails.pool;
         this.memberDetails = memberDetails;
         this.poolDetails = poolDetails;
-        this.updateAddWithdrawnValueInRune(actions, assetPriceInRuneByTime);
+        this.updateAddWithdrawnValueInRune(actions, assetPriceInRuneByTime, assetPriceInUsdByTime);
 
         // TODO(leifthelucky): Check how to divide this correctly without losing precision.
         this.redeemableRune = memberDetails.liquidityUnits / poolDetails.liquidityUnits * poolDetails.runeDepth;
         this.redeemableAsset = memberDetails.liquidityUnits / poolDetails.liquidityUnits * poolDetails.assetDepth;
 
         this.realizedReturnValueInRune = this.withdrawnValueInRune - this.addedValueInRune;
+        this.realizedReturnValueInAsset = this.withdrawnValueInAsset - this.addedValueInAsset;
+        this.realizedReturnValueInUsd = this.withdrawnValueInUsd - this.addedValueInUsd;
+
         this.reedeemableValueInRune = this.redeemableRune + this.redeemableAsset * poolDetails.assetPrice;
+        this.reedeemableValueInAsset = this.redeemableRune / poolDetails.assetPrice + this.redeemableAsset;
+        this.reedeemableValueInUsd = this.redeemableRune / poolDetails.assetPrice * poolDetails.assetPriceUSD + this.redeemableAsset * poolDetails.assetPriceUSD;
+
         this.totalReturnValueInRune = this.realizedReturnValueInRune + this.reedeemableValueInRune;
+        this.totalReturnValueInAsset = this.realizedReturnValueInAsset + this.reedeemableValueInAsset;
+        this.totalReturnValueInUsd = this.realizedReturnValueInUsd + this.reedeemableValueInUsd;
     }
-    updateAddWithdrawnValueInRune(actions, assetPriceInRuneByTime) {
+    updateAddWithdrawnValueInRune(actions, assetPriceInRuneByTime, assetPriceInUsdByTime) {
         this.addedRune = 0;
         this.addedAsset = 0;
         this.addedValueInRune = 0;
+        this.addedValueInAsset = 0;
+        this.addedValueInUsd = 0;
         this.withdrawnRune = 0;
         this.withdrawnAsset = 0;
         this.withdrawnValueInRune = 0;
+        this.withdrawnValueInAsset = 0;
+        this.withdrawnValueInUsd = 0;
         for (const action of actions.actions) {
             if (action.status != "success") {
                 continue;
@@ -47,36 +59,68 @@ class LPLiquidity {
             if (action.pools.length != 1 || action.pools[0] != this.pool) {
                 continue;
             }
-            const valueInRune = function (coin, assetFilter, assetPrice) {
+            const valueInRune = function (coin, assetFilter, assetPriceRune) {
                 switch (coin.asset) {
                     case "THOR.RUNE":
                         return Number(coin.amount);
                     case assetFilter:
-                        return Number(coin.amount) * assetPrice;
+                        return Number(coin.amount) * assetPriceRune;
+                    default:
+                        return;
+                }
+            }
+            const valueInAsset = function (coin, assetFilter, assetPriceRune) {
+                switch (coin.asset) {
+                    case "THOR.RUNE":
+                        return Number(coin.amount) / assetPriceRune;
+                    case assetFilter:
+                        return Number(coin.amount);
+                    default:
+                        return;
+                }
+            }
+            const valueInUsd = function (coin, assetFilter, assetPriceRune, assetPriceUsd) {
+                console.log(coin.amount, assetPriceRune, assetPriceUsd)
+                switch (coin.asset) {
+                    case "THOR.RUNE":
+                        return Number(coin.amount) / assetPriceRune * assetPriceUsd;
+                    case assetFilter:
+                        return Number(coin.amount) * assetPriceUsd;
                     default:
                         return;
                 }
             }
             const s = Math.floor(action.date / 1e9);
-            const assetPrice = assetPriceInRuneByTime[s];
+            const assetPriceRune = assetPriceInRuneByTime[s];
+            console.log(assetPriceInUsdByTime);
+            console.log(s);
+            const assetPriceUsd = assetPriceInUsdByTime[s];
             let inRune = 0;
             let inAsset = 0;
             let inValueInRune = 0;
+            let inValueInAsset = 0;
+            let inValueInUsd = 0;
             let outRune = 0;
             let outAsset = 0;
             let outValueInRune = 0;
+            let outValueInAsset = 0;
+            let outValueInUsd = 0;
             for (const inputs of action.in) {
                 for (const coin of inputs.coins) {
                     inRune += (coin.asset == "THOR.RUNE") ? Number(coin.amount) : 0;
                     inAsset += (coin.asset == this.pool) ? Number(coin.amount) : 0;
-                    inValueInRune += valueInRune(coin, this.pool, assetPrice);
+                    inValueInRune += valueInRune(coin, this.pool, assetPriceRune);
+                    inValueInAsset += valueInAsset(coin, this.pool, assetPriceRune);
+                    inValueInUsd += valueInUsd(coin, this.pool, assetPriceRune, assetPriceUsd);
                 }
             }
             for (const inputs of action.out) {
                 for (const coin of inputs.coins) {
                     outRune += (coin.asset == "THOR.RUNE") ? Number(coin.amount) : 0;
                     outAsset += (coin.asset == this.pool) ? Number(coin.amount) : 0;
-                    outValueInRune += valueInRune(coin, this.pool, assetPrice);
+                    outValueInRune += valueInRune(coin, this.pool, assetPriceRune);
+                    outValueInAsset += valueInAsset(coin, this.pool, assetPriceRune);
+                    outValueInUsd += valueInUsd(coin, this.pool, assetPriceRune, assetPriceUsd);
                 }
             }
             switch (action.type) {
@@ -86,6 +130,8 @@ class LPLiquidity {
                     this.withdrawnRune += outRune - inRune;
                     this.withdrawnAsset += outAsset - inAsset;
                     this.withdrawnValueInRune += outValueInRune - inValueInRune;
+                    this.withdrawnValueInAsset += outValueInAsset - inValueInAsset;
+                    this.withdrawnValueInUsd += outValueInUsd - inValueInUsd;
                     break;
                 case "addLiquidity":
                     // Out rune/asset/value should be 0 for addLiquidity events,
@@ -93,6 +139,8 @@ class LPLiquidity {
                     this.addedRune += inRune - outRune;
                     this.addedAsset += inAsset - outAsset;
                     this.addedValueInRune += inValueInRune - outValueInRune;
+                    this.addedValueInAsset += inValueInAsset - outValueInAsset;
+                    this.addedValueInUsd += inValueInUsd - outValueInUsd;
                     break;
             }
         }
