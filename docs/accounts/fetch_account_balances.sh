3815566e8415b9fde0e6569e39cc5ffc2bcaa8e3
Freki <freki.tc@protonmail.com>
2022-04-13 14:00:30 +0200

    Fix parameter order in fetch_account_balances.sh

--- docs/accounts/fetch_account_balances.sh
+++ docs/accounts/fetch_account_balances.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 address=$1
-server=${2:-https://thornode.ninerealms.com}
+height="x-cosmos-block-height: $2"
+server=${3:-https://thornode.ninerealms.com}
 endpoint="cosmos/bank/v1beta1/balances"
-height="x-cosmos-block-height: $3"
 curl --insecure ${server}/${endpoint}/${address} -H "${height}"
