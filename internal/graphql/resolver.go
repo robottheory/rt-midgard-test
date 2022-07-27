20f38065bb65cd9267b667cd5f9d3b69b5694df1
HooriRn <hoorirn@protonmail.com>
2022-07-27 09:46:15 +0100

    Delete graphql files

--- internal/graphql/resolver.go
+++
@@ -1,7 +0,0 @@
-//go:generate go run github.com/99designs/gqlgen
-
-package graphql
-
-type Resolver struct{}
-
-// TODO cache repeated db calls to improve efficiency like stat.PoolStakesLookup, UnstakeLookup etc
